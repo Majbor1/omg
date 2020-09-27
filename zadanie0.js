55cd1b814d8d1650eee9330eac50b2e6f6a3491c
Mona Lisa <marcinmajborski@gmail.com>
2020-09-27 22:30:33 +0200

    init

---
+++ zadanie0.js
@@ -0,0 +1,2 @@
+console.log("witaj świecie");
+//Marcin Majborski
---
+++ zadanie1a.js
@@ -0,0 +1,4 @@
+document.write("<h1>Marcin Majborski</h1>" );
+        document.write("<h2>Technik Programista 2PIR</h2><HR>" );
+            console.log("Marcin Majborski");
+        alert("Witam na mojej stronie!!");
